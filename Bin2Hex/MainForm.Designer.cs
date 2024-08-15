d766ac02130153519645dc82e3d22deb1c10dcb9
Ferhat Mousavi <ferhat.mousavi@gmail.com>
2024-08-15 16:25:57 +0300

    Upload the project

---
+++ Bin2Hex/MainForm.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Bin2Hex
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOpenFile = new System.Windows.Forms.Button();
+            this.openFileDialogMain = new System.Windows.Forms.OpenFileDialog();
+            this.checkBoxInverted = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // buttonOpenFile
+            // 
+            this.buttonOpenFile.Location = new System.Drawing.Point(12, 43);
+            this.buttonOpenFile.Name = "buttonOpenFile";
+            this.buttonOpenFile.Size = new System.Drawing.Size(205, 75);
+            this.buttonOpenFile.TabIndex = 0;
+            this.buttonOpenFile.Text = "Open File";
+            this.buttonOpenFile.UseVisualStyleBackColor = true;
+            this.buttonOpenFile.Click += new System.EventHandler(this.buttonOpenFile_Click);
+            // 
+            // openFileDialogMain
+            // 
+            this.openFileDialogMain.InitialDirectory = ".";
+            this.openFileDialogMain.Title = "Open Binary File";
+            // 
+            // checkBoxInverted
+            // 
+            this.checkBoxInverted.AutoSize = true;
+            this.checkBoxInverted.Location = new System.Drawing.Point(12, 12);
+            this.checkBoxInverted.Name = "checkBoxInverted";
+            this.checkBoxInverted.Size = new System.Drawing.Size(81, 21);
+            this.checkBoxInverted.TabIndex = 1;
+            this.checkBoxInverted.Text = "Inverted";
+            this.checkBoxInverted.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 130);
+            this.Controls.Add(this.checkBoxInverted);
+            this.Controls.Add(this.buttonOpenFile);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Bin2Hex";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOpenFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialogMain;
+        private System.Windows.Forms.CheckBox checkBoxInverted;
+    }
+}
+
---
+++ Bin2Hex/MainForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bin2Hex
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOpenFile_Click(object sender, EventArgs e)
+        {
+            openFileDialogMain.InitialDirectory = Environment.CurrentDirectory;
+            if (openFileDialogMain.ShowDialog() == DialogResult.OK)
+            {
+                string strBinaryFileNameAndPath = openFileDialogMain.FileName;
+                string strBinaryFileName = openFileDialogMain.SafeFileName;
+                string strHexFileNameAndPath = string.Format("{0}.hex", strBinaryFileNameAndPath);
+                string strHexFileName = string.Format("{0}.hex", strBinaryFileName);
+
+                byte[] byteBinaryFileContent = File.ReadAllBytes(strBinaryFileNameAndPath);
+                int iBinaryFileLength = byteBinaryFileContent.Length;
+
+                using (StreamWriter xStreamWriter = new StreamWriter(strHexFileNameAndPath))
+                {
+                    xStreamWriter.WriteLine("//********************************************************");
+                    xStreamWriter.WriteLine("// Binary file coverter to Hex C structure");
+                    xStreamWriter.WriteLine("// Version 1.00, (c) Evieplus");
+                    xStreamWriter.WriteLine(string.Format("// Binary File Name    : {0}", strBinaryFileName));
+                    xStreamWriter.WriteLine(string.Format("// Binary File Length  : {0}", iBinaryFileLength));
+                    xStreamWriter.WriteLine("//********************************************************");
+                    xStreamWriter.WriteLine("unsigned char ucaBin2Hex[]=");
+                    xStreamWriter.WriteLine("{");
+
+                    string strLineContent = "\t"; ;
+                    byte byteValue = 0x00;
+
+                    for (int iIndex = 0; iIndex < iBinaryFileLength; iIndex++)
+                    {
+                        if (checkBoxInverted.Checked == true)
+                            byteValue = (byte)~byteBinaryFileContent[iIndex];
+                        else
+                            byteValue = byteBinaryFileContent[iIndex];
+
+                        strLineContent += string.Format("0x{0:X02}, ", byteValue);
+
+                        if ((iIndex + 1) % 16 == 0)
+                        {
+                            xStreamWriter.WriteLine(strLineContent);
+                            strLineContent = "\t";
+                        }
+                    }
+
+                    if (strLineContent != "\t")
+                    {
+                        xStreamWriter.WriteLine(strLineContent);
+                    }
+
+                    xStreamWriter.WriteLine("};");
+
+                    MessageBox.Show("Converted");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Can not open file!");
+            }
+        }
+    }
+}
